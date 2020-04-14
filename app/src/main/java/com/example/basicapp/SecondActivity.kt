65c292ce28cf88fbfb07d486272b49690617949d
Paul Ridgway <paul@ridgway.io>
2020-04-14 21:18:27 +0100

    Search google

--- app/src/main/java/com/example/basicapp/SecondActivity.kt
+++ app/src/main/java/com/example/basicapp/SecondActivity.kt
@@ -1,12 +1,23 @@
 package com.example.basicapp
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.second_activity.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
+
+        button2.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_WEB_SEARCH
+            intent.putExtra(SearchManager.QUERY, editText.text.toString())
+            startActivity(intent)
+        }
     }
 }
